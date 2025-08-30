a0e95e034dd53dd9726903157f7aec8914bb136d
PantheraNestah <pantheranestah@gmail.com>
2025-08-30 23:00:43 +0300

    feat: implement a view that retrieves all property listings from the database

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Property
+import logging
 
 # Create your views here.
+
+# Set up a logger to see cache hits/misses
+logger = logging.getLogger(__name__)
+
+@cache_page(60 * 15)  # Cache this view for 15 minutes
+def property_list(request):
+    """
+    This view retrieves all properties from the database.
+    The first time it's accessed, it hits the DB and its response is cached.
+    Subsequent requests within 15 minutes will receive the cached response.
+    """
+    properties = Property.objects.all()
+    
+    # This log will only appear on a "cache miss"
+    logger.warning("DATABASE HIT: The property_list view was executed.")
+    
+    context = {
+        'properties': properties
+    }
+    return render(request, 'properties/property_list.html', context)
